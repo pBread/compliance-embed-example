e6606bf9ed7cb892649823859dacada28cc2b5d4
pBread <4473570+pBread@users.noreply.github.com>
2025-06-05 17:59:05 -0600

    it's working!

--- pages/api/token.ts
+++ pages/api/token.ts
@@ -1,30 +1,19 @@
-import type { NextApiHandler } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import twilio from "twilio";
 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, DEMO_PHONE, DEMO_EMAIL } =
   process.env;
 
-if (!TWILIO_ACCOUNT_SID) throw Error("missing env TWILIO_ACCOUNT_SID");
-if (!TWILIO_AUTH_TOKEN) throw Error("missing env TWILIO_AUTH_TOKEN");
-if (!DEMO_PHONE) throw Error("missing DEMO_PHONE");
-if (!DEMO_EMAIL) throw Error("missing DEMO_EMAIL");
-
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-const createComplianceTollfreeInquiry = async () => {
-  const inqiry = await client.trusthub.v1.complianceTollfreeInquiries.create({
-    notificationEmail: DEMO_EMAIL,
-    tollfreePhoneNumber: DEMO_PHONE,
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const inquiry = await client.trusthub.v1.complianceTollfreeInquiries.create({
+    notificationEmail: DEMO_EMAIL!,
+    tollfreePhoneNumber: DEMO_PHONE!,
   });
 
-  console.log(inqiry.inquiryId);
-
-  return inqiry;
-};
-
-const handler: NextApiHandler = async (req, res) => {
-  const inquiry = await createComplianceTollfreeInquiry();
-  res.status(200).json({ inquiry });
-};
-
-export default handler;
+  res.status(200).json(inquiry);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,28 @@
+import { TwilioComplianceEmbed } from "@twilio/twilio-compliance-embed";
 import { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-
-const TwilioComplianceEmbed = dynamic(
-  () =>
-    import("@twilio/twilio-compliance-embed").then((item) => {
-      console.debug("import dynamic", Object.keys(item));
-      return item.TwilioComplianceEmbed;
-    }),
-  { ssr: false },
-);
-
-interface Inquiry {
-  inquiryId: string;
-  inquirySessionToken: string;
-}
 
 export default function HomePage() {
   const [inquiry, setInquiry] = useState<Inquiry>();
 
   useEffect(() => {
-    const getSetToken = async () => {
-      const token = await fetch("/api/token").then((res) => res.json());
-      setInquiry(token);
-    };
-
-    console.log("getSetToken");
-
-    getSetToken();
+    fetch("/api/token")
+      .then((res) => res.json())
+      .then(setInquiry);
   }, []);
-
-  console.debug("inquiry", inquiry);
-
-  console.debug("TwilioComplianceEmbed", TwilioComplianceEmbed);
 
   if (!inquiry) return <div>Loading</div>;
 
   return (
-    <div style={{ width: "500px", height: "500px" }}>
+    <div style={{ width: "800px", minHeight: "500px", height: "100vh" }}>
       <TwilioComplianceEmbed
         inquiryId={inquiry.inquiryId}
         inquirySessionToken={inquiry.inquirySessionToken}
       />
     </div>
   );
+}
+
+interface Inquiry {
+  inquiryId: string;
+  inquirySessionToken: string;
 }
