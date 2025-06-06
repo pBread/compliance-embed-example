3070cca04d0a101128487a2933c30269e2a8f98e
pBread <4473570+pBread@users.noreply.github.com>
2025-06-05 19:56:56 -0600

    added header

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <img src="/logo.png" className="logo" />
+    </header>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
+import { Header } from "@/components/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main>
+    <>
       <Head>
         <title>Compliance Embed</title>
       </Head>
-      <Component {...pageProps} />
-    </main>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
